40cbea0d1d9944108e7d4a78f5d77e50f496bdb3
people call me geet <makcrtve@gmail.com>
2026-01-02 20:13:59 +0800

    Add files via upload

--- build.bat
+++ build.bat
@@ -25,16 +25,20 @@ echo.
 echo [3/4] Membuat file EXE (Mungkin butuh semenit...)...
 python -m PyInstaller --log-level WARN --noconsole --onefile %ICON_ARG% --add-data "icon.ico;." --version-file="file_version_info.txt" --name "UniversalDiscordRPC" main.py
 
-echo.
 echo [4/4] Menyelesaikan proses akhir...
 if not exist dist mkdir dist
 copy config.json dist\ >nul
 
 echo.
+echo [Bonus] Membuat paket Rilis (ZIP)...
+powershell -Command "Compress-Archive -Path 'dist\*' -DestinationPath 'UDRPC_v1.2_Release.zip' -Force"
+
+echo.
 echo ==========================================
 echo    BUILD BERHASIL!
 echo ==========================================
-echo Lokasi File: \dist\UniversalDiscordRPC.exe
+echo Lokasi EXE:   \dist\UniversalDiscordRPC.exe
+echo Lokasi Rilis: \UDRPC_v1.2_Release.zip
 echo.
 echo TIP: Jika icon tetap tidak muncul, coba rename file .exe-nya atau copy ke folder Desktop.
 echo.
